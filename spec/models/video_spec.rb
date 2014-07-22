03b18fbc4650b4d2e641b942475cb364e888be5e
Michael P. Martin <mmartin@levsrin.com>
2014-07-21 20:01:55 -0400

    add model specs

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,12 +4,13 @@ require 'rails_helper'
 RSpec.describe Video, type: :model do
 
   it "saves itself" do
-    v = Video.new(title: 'My Video Title', description: 'My lengthy video description.', poster_small_url: 'my_small_cover_url.jpg', poster_large_url: 'my_large_cover_url.jpg')
-    v.save
+    new_video = Video.new(title: 'My Video Title', description: 'My lengthy video description.', poster_small_url: 'my_small_cover_url.jpg', poster_large_url: 'my_large_cover_url.jpg')
+    new_video.save
 
-    expect(v.title).to eq('My Video Title')
-    expect(v.description).to eq('My lengthy video description.')
-    expect(v.poster_small_url).to eq('my_small_cover_url.jpg')
-    expect(v.poster_large_url).to eq('my_large_cover_url.jpg')
+    saved_video = Video.first
+    expect(saved_video.title).to eq('My Video Title')
+    expect(saved_video.description).to eq('My lengthy video description.')
+    expect(saved_video.poster_small_url).to eq('my_small_cover_url.jpg')
+    expect(saved_video.poster_large_url).to eq('my_large_cover_url.jpg')
   end
 end
