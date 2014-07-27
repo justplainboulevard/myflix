dc464f55c832c2f1544ba2828e91f1f3959c487a
Michael P. Martin <mmartin@levsrin.com>
2014-07-27 15:06:18 -0400

    fix has_secure_password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 
 class User < ActiveRecord::Base
 
-  has_secure_password
+  has_secure_password validations: false
 
   validates_presence_of :email_address, :password, :full_name
   validates_uniqueness_of :email_address
