061efa197b4c4d83d73c5e59f48f5ac70ac5ced5
Michael P. Martin <mmartin@levsrin.com>
2014-10-04 18:03:49 -0400

    attempt deployment error fix

--- app/forms/create_video.rb
+++ app/forms/create_video.rb
@@ -1,52 +1,52 @@
 
-module Form
+# module Form
 
-  class CreateVideo
+#   class CreateVideo
 
-    include Virtus
+#     include Virtus
 
-    extend ActiveModel::Naming
-    include ActiveModel::Conversion
-    include ActiveModel::Validations
+#     extend ActiveModel::Naming
+#     include ActiveModel::Conversion
+#     include ActiveModel::Validations
 
-    attribute :title, String
-    attribute :description, Text
-    attribute :category_ids, Array
+#     attribute :title, String
+#     attribute :description, Text
+#     attribute :category_ids, Array
 
-    validates :title, presence: true, length: { maximum: 150 }
-    validates :description, presence: true, length: { maximum: 1500 }
+#     validates :title, presence: true, length: { maximum: 150 }
+#     validates :description, presence: true, length: { maximum: 1500 }
 
-    def persisted?
-      false
-    end
+#     def persisted?
+#       false
+#     end
 
-    def save
-      if valid?
-        persist!
-        true
-      else
-        false
-      end
-    end
+#     def save
+#       if valid?
+#         persist!
+#         true
+#       else
+#         false
+#       end
+#     end
 
-  private
+#   private
 
-    def persist!
-      @video = Video.create!(title: title, description: description)
-      category_ids.each do |category_id|
-        VideoCategory.create!(category_id: category_id, video_id: @video.id)
-      end
-    end
-  end
+#     def persist!
+#       @video = Video.create!(title: title, description: description)
+#       category_ids.each do |category_id|
+#         VideoCategory.create!(category_id: category_id, video_id: @video.id)
+#       end
+#     end
+#   end
 
-  # ALTERNATIVE APPROACH USING TRANSACTION:
-  # def create_objects
-  #   ActiveRecord::Base.transaction do
-  #     user.save!
-  #     business.save!
-  #     membership.save!
-  #   end
-  # rescue
-  #   false
-  # end
-end
+#   # ALTERNATIVE APPROACH USING TRANSACTION:
+#   # def create_objects
+#   #   ActiveRecord::Base.transaction do
+#   #     user.save!
+#   #     business.save!
+#   #     membership.save!
+#   #   end
+#   # rescue
+#   #   false
+#   # end
+# end
