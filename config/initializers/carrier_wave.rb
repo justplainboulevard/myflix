bfae05c23d9d91c646cd9b04884fcf21a9ff0c66
Michael P. Martin <mmartin@levsrin.com>
2014-10-04 18:44:38 -0400

    attempt deployment error fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,11 +7,11 @@ CarrierWave.configure do |config|
 
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Figaro.env.aws_access_key_id,
-      aws_secret_access_key: Figaro.env.aws_secret_access_key
+      aws_access_key_id: ENV['aws_access_key_id'],
+      aws_secret_access_key: ENV['aws_secret_access_key']
     }
 
-    config.fog_directory = Figaro.env.s3_bucket_name
+    config.fog_directory = ENV['s3_bucket_name']
   else
 
     config.storage = :file
