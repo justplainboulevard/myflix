b154be05d0f1208ea25daf95280056896fc34509
Michael P. Martin <mmartin@levsrin.com>
2014-10-04 18:25:17 -0400

    attempt deployment error fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,11 +7,11 @@ CarrierWave.configure do |config|
 
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AWS_ACCESS_KEY_ID',
-      aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY'
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
 
-    config.fog_directory = 'S3_BUCKET_NAME'
+    config.fog_directory = ENV['S3_BUCKET_NAME']
   else
 
     config.storage = :file
